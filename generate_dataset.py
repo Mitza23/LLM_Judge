e909cd215966e5a63aaa3f2183bf34e353d83f27
Mitza23 <mitzagrebla@yahoo.com>
2025-06-07 12:28:35 +0300

    Add generation scripts

--- generate_dataset.py
+++ generate_dataset.py
@@ -3,8 +3,13 @@ from deepeval.synthesizer import Synthesizer
 from model import CustomMistral7B
 
 synthesizer = Synthesizer(model=CustomMistral7B())
-dataset.generate_goldens_from_docs(
-    synthesizer=synthesizer,
+
+synthesizer.generate_goldens_from_docs(
     document_paths=['docs/Guidelines-non_std_complex.txt'],
-    max_goldens_per_document=2
+    include_expected_output=True
+)
+
+synthesizer.save_as(file_type="csv",
+                    file_name="deepeval-dataset",
+                    directory="./deepeval-dataset"
 )
