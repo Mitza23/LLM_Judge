6c24854677579da048ddc8b780632f6f3f068e7a
Mitza23 <mitzagrebla@yahoo.com>
2025-06-08 19:31:32 +0300

    Adjust model

--- model.py
+++ model.py
@@ -1,72 +1,37 @@
-import json
-from pydantic import BaseModel
-import torch
-from lmformatenforcer import JsonSchemaParser
-from lmformatenforcer.integrations.transformers import (
-    build_transformers_prefix_allowed_tokens_fn,
-)
-from transformers import BitsAndBytesConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from deepeval.models.base_model import DeepEvalBaseLLM
 
-from deepeval.models import DeepEvalBaseLLM
-
-
-class CustomMistral7B(DeepEvalBaseLLM):
-    def __init__(self):
-        quantization_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.float16,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,
-        )
-
-        model_4bit = AutoModelForCausalLM.from_pretrained(
-            "mistralai/Mistral-7B-Instruct-v0.3",
-            device_map="auto",
-            quantization_config=quantization_config,
-        )
-        tokenizer = AutoTokenizer.from_pretrained(
-            "mistralai/Mistral-7B-Instruct-v0.3"
-        )
-
-        self.model = model_4bit
+class Mistral7B(DeepEvalBaseLLM):
+    def __init__(
+            self,
+            model,
+            tokenizer
+    ):
+        self.model = model
         self.tokenizer = tokenizer
 
     def load_model(self):
         return self.model
 
-    def generate(self, prompt: str, schema: BaseModel) -> BaseModel:
+    def generate(self, prompt: str) -> str:
         model = self.load_model()
-        pipeline = pipeline(
-            "text-generation",
-            model=model,
-            tokenizer=self.tokenizer,
-            use_cache=True,
-            device_map="auto",
-            max_length=2500,
-            do_sample=True,
-            top_k=5,
-            num_return_sequences=1,
-            eos_token_id=self.tokenizer.eos_token_id,
-            pad_token_id=self.tokenizer.eos_token_id,
-        )
 
-        # Create parser required for JSON confinement using lmformatenforcer
-        parser = JsonSchemaParser(schema.model_json_schema())
-        prefix_function = build_transformers_prefix_allowed_tokens_fn(
-            pipeline.tokenizer, parser
-        )
+        device = "cuda" # the device to load the model onto
 
-        # Output and load valid JSON
-        output_dict = pipeline(prompt, prefix_allowed_tokens_fn=prefix_function)
-        output = output_dict[0]["generated_text"][len(prompt) :]
-        json_result = json.loads(output)
+        model_inputs = self.tokenizer([prompt], return_tensors="pt").to(device)
+        model.to(device)
 
-        # Return valid JSON object according to the schema DeepEval supplied
-        return schema(**json_result)
+        generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
+        return self.tokenizer.batch_decode(generated_ids)[0]
 
-    async def a_generate(self, prompt: str, schema: BaseModel) -> BaseModel:
-        return self.generate(prompt, schema)
+    async def a_generate(self, prompt: str) -> str:
+        return self.generate(prompt)
 
     def get_model_name(self):
-        return "Mistral-7B v0.3"
+        return "Mistral-7B-Instruct-v0.3"
+
+# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+#
+# mistral_7b = Mistral7B(model=model, tokenizer=tokenizer)
+# print(mistral_7b.generate("Write me a joke"))
