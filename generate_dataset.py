2109f2b94d4039933c3327dd59b715866d801c51
Mitza23 <mitzagrebla@yahoo.com>
2025-06-08 19:34:32 +0300

    Adjust dataset generator

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,14 +1,19 @@
 from deepeval.dataset import EvaluationDataset, dataset
 from deepeval.synthesizer import Synthesizer
 from deepeval.synthesizer.config import ContextConstructionConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from embedding_model import CustomEmbeddingModel
 from model import CustomMistral7B
 
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
 
-synthesizer = Synthesizer(model=CustomMistral7B())
+mistral_7b = CustomMistral7B(model=model, tokenizer=tokenizer)
 
-context_config = ContextConstructionConfig(embedder=CustomEmbeddingModel(), critic_model=CustomMistral7B())
+synthesizer = Synthesizer(model=mistral_7b)
+
+context_config = ContextConstructionConfig(embedder=CustomEmbeddingModel(), critic_model=mistral_7b)
 synthesizer.generate_goldens_from_docs(
     document_paths=['docs/Guidelines-non_std_complex.txt'],
     include_expected_output=True,
